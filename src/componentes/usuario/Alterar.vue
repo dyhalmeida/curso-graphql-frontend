2d344303816ec53d9cf5897b61dd64a5a65ae150
Diego Almeida <dyhalmeida@gmail.com>
2022-03-26 12:39:20 -0300

    wip: update profile

--- src/componentes/usuario/Alterar.vue
+++ src/componentes/usuario/Alterar.vue
@@ -46,7 +46,7 @@
                         <v-text-field label="ID" readonly
                             v-model="dados.id" />
                         <v-text-field label="Nome" readonly
-                            v-model="dados.nome" />
+                            v-model="dados.name" />
                         <v-text-field label="E-mail" readonly
                             v-model="dados.email" />
                         <v-text-field label="Perfis" readonly
@@ -75,12 +75,12 @@ export default {
     },
     computed: {
         perfisRotulos() {
-            return this.dados && this.dados.perfis &&
-                this.dados.perfis.map(p => p.rotulo).join(', ')
+            return this.dados && this.dados.profiles &&
+                this.dados.profiles.map(p => p.role).join(', ')
         },
         perfisSelecionados() {
-            if(this.usuario.perfis) {
-                return this.usuario.perfis.map(id => ({ id }))
+            if(this.usuario.profiles) {
+                return this.usuario.profiles.map(id => ({ id }))
             } else {
                 return null
             }
@@ -88,7 +88,52 @@ export default {
     },
     methods: {
         alterarUsuario() {
-            // Implementar
+            // this.$api.mutate({
+            //     mutation: gql`
+            //         mutation(
+            //             $id: ID
+            //             $email: String
+            //             $name: String
+            //             $email: String
+            //             $password: String
+            //             $profiles: [ProfileFilters]
+            //         ){
+            //             updateUser(
+            //                 data: {
+            //                     name: $name
+            //                     email: $email
+            //                     password: $password
+            //                     profiles: $profiles
+            //                 }
+            //                 filters: {
+            //                     id: $idFilter
+            //                     email: $emailFilter
+            //                 }
+            //             ) {
+            //                 id name email profiles { role }
+            //             }
+            //         }
+            //     `,
+            //     variables: {
+            //        data: {
+            //             name: this.usuario.nome,
+            //             email: this.usuario.email,
+            //             password: this.usuario.password,
+            //             profiles: this.perfisSelecionados
+            //        },
+            //        filters: {
+            //            id: this.filtro.id,
+            //            email: this.filtro.email
+            //        }
+            //     }
+            // }).then(response => {
+            //     this.dados = response.data.updateUser
+            //     this.filtro = {}
+            //     this.usuario = {}
+            //     this.erros = null
+            // }).catch(e => {
+            //     this.erros = e
+            // })
         },
         obterPerfis() {
             this.$api.query({
